8e84b4dc7d63fb96cd2c7495d74f7ae26a2796c2
aterui <hanabi0111@gmail.com>
2022-10-13 18:28:42 -0400

    rmd

--- code/analysis_pr_area.R
+++ code/analysis_pr_area.R
@@ -29,3 +29,23 @@ df_pr <- df_str %>%
   rename(river = name)
 
 cor_pa <- cor.test(df_pr$pr, df_pr$area, method = "spearman")
+
+
+# plot --------------------------------------------------------------------
+
+g_pra <- df_pr %>% 
+  ggplot(aes(x = area,
+             y = pr)) +
+  geom_point() +
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10") +
+  labs(color = "River",
+       alpha = expression(log[10]~italic(N[L])),
+       y = expression("Branching ratio"~p[r]~"[km"^"-1"*"]"),
+       x = expression("Watershed area"~A~"[km"^"2"*"]")) +
+  theme_bw() +
+  theme(panel.grid = element_blank())
+
+ggsave(g_pra,
+       filename = here::here("output/figure_pra.pdf"),
+       height = 4, width = 5)
