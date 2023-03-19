fa24b853773031bc0f9c4fc9ad6f2f69e3cf3228
AkiraTerui <hanabi0111@gmail.com>
2023-03-18 22:54:38 -0400

    Update analysis_pr_all.R

--- code/analysis_pr_all.R
+++ code/analysis_pr_all.R
@@ -1,6 +1,7 @@
 
 # setup -------------------------------------------------------------------
 
+rm(list = ls())
 source(here::here("code/library.R"))
 source(here::here("code/set_functions.R"))
 
@@ -17,7 +18,7 @@ df_str <- readRDS(here::here("data_fmt/epsg4326_strnet.rds")) %>%
 
 # pr ----------------------------------------------------------------------
 
-df_pr <- df_str %>% 
+df_p <- df_str %>% 
   mutate(length = as.numeric(length)) %>% 
   group_by(name, a_t) %>% 
   summarize(n = n(),
@@ -26,22 +27,18 @@ df_pr <- df_str %>%
   ungroup() %>% 
   mutate(prop_a = a_t / area,
          pr = n / tl,
+         p = 1 - exp(-pr),
          n = as.numeric(n)) %>% 
   rename(river = name)
 
 
 # model fitting -----------------------------------------------------------
 
-fit0 <- MASS::rlm(log(pr, 10) ~ log(a_t, 10) + river - 1,
-                  data = df_pr)
+fit0 <- MASS::rlm(log(p, 10) ~ log(a_t, 10) + river - 1,
+                  data = df_p)
 
-fit1 <- MASS::rlm(log(pr, 10) ~ log(a_t, 10) * river - 1,
-                  df_pr)
-
-BF_all <- exp((BIC(fit1) - BIC(fit0))/2)
-  
-## data frame for prediction
-X <- df_pr %>% 
+## data frame for pediction
+X <- df_p %>% 
   group_by(river) %>% 
   summarize(a_t = seq(min(a_t), max(a_t), length = 10)) %>% 
   ungroup()
@@ -49,20 +46,20 @@ X <- df_pr %>%
 y <- predict(fit0, X)
 
 df_pred <- X %>% 
-  mutate(pr = 10^y,
-         pr_prime = 10^y / a_t^coef(fit0)[1]) %>% 
-  left_join(df_pr %>% 
+  mutate(p = 10^y,
+         p_prime = 10^y / a_t^coef(fit0)[1]) %>% 
+  left_join(df_p %>% 
               distinct(river, prop_a),
             by = "river")
 
-df_pr <- df_pr %>% 
-  mutate(pr_prime = pr / a_t^coef(fit0)[1])
+df_p <- df_p %>% 
+  mutate(p_prime = p / a_t^coef(fit0)[1])
 
 
 # plot --------------------------------------------------------------------
 
 ## color setup
-river <- unique(df_pr$river)
+river <- unique(df_p$river)
 
 ex_river <- c("KleineEmme",
               "Chisone",
@@ -87,10 +84,10 @@ names(cols) <- v_river
 r <- 0.75
 lwd <- 0.2
 
-## plot: raw pr
-g_pr <- df_pr %>%
+## plot: raw p
+g_p <- df_p %>%
   ggplot(aes(x = a_t,
-             y = pr,
+             y = p,
              color = river)) +
   geom_point(data = . %>% filter(!(river %in% ex_river)),
              aes(alpha = log(n, 10)),
@@ -109,67 +106,15 @@ g_pr <- df_pr %>%
                      labels = ex_river) +
   labs(color = "River",
        alpha = expression(log[10]~italic(N[L])),
-       y = expression("Branching ratio"~p[r]~"[km"^"-1"*"]"),
+       y = expression("Branching probability"~p~"[km"^"-1"*"]"),
        x = expression("Observation scale"~A[T]~"[km"^"2"*"]")) +
   theme_bw() +
   theme(panel.grid = element_blank())
 
-g_pr_facet <- g_pr +
-  facet_wrap(facets = ~river, 10, 5) +
-  guides(color = "none") +
-  theme(strip.background = element_blank())
-
-## plot: non-dimensional pr
-g_prp <- df_pr %>%
-  ggplot(aes(x = prop_a * 100,
-             y = pr_prime,
-             color = river)) +
-  geom_point(data = . %>% filter(!(river %in% ex_river)),
-             aes(alpha = log(n, 10)),
-             size = r) +
-  geom_line(data = df_pred %>% filter(!(river %in% ex_river)),
-            size = lwd) +
-  geom_point(data = . %>% filter(river %in% ex_river),
-             aes(alpha = log(n, 10)),
-             size = r) +
-  geom_line(data = df_pred %>% filter(river %in% ex_river),
-            size = lwd) +
-  scale_x_continuous(trans = "log10", labels = scales::comma) +
-  scale_y_continuous(trans = "log10") +
-  scale_color_manual(values = cols,
-                     breaks = ex_river,
-                     labels = ex_river) +
-  labs(color = "River",
-       alpha = expression(log[10]~italic(N[L])),
-       y = expression("Rescaled branching ratio"~bar(p)[r]~"[-]"),
-       x = expression("% observation scale"~A[T]~"/A [%]")) +
-  theme_bw() +
-  theme(panel.grid = element_blank())
-
-g_prp_facet <- g_prp +
-  facet_wrap(facets = ~river, 10, 5) +
-  guides(color = "none") +
-  theme(strip.background = element_blank())
-
 
 # export ------------------------------------------------------------------
 
-g_all <- g_pr + g_prp + 
-  plot_annotation(tag_levels = "A") +
-  plot_layout(guides = "collect") &
-  theme(legend.position = "bottom")
-
-ggsave(g_pr,
-       filename = here::here("output/figure_pr_all.pdf"),
+ggsave(g_p,
+       filename = here::here("output/figure_p.pdf"),
        height = 6,
        width = 8)
-
-ggsave(g_pr_facet,
-       filename = here::here("output/figure_pr_facet.pdf"),
-       height = 11,
-       width = 11)
-
-ggsave(g_prp_facet,
-       filename = here::here("output/figure_prp_facet.pdf"),
-       height = 11,
-       width = 11)
